662ac4ebb1c963d716116d37430270fd6d7cee60
Md Hasan Raza <hasanraza@Mds-MacBook-Air.local>
2025-08-28 18:08:12 +0530

    chore: allow network access for Vite dev server (host 0.0.0.0)

--- first/src/shaders/fragment.glsl
+++ first/src/shaders/fragment.glsl
@@ -1,9 +1,13 @@
+uniform sampler2D uTexture;
+
+varying vec2 vUv;
 
 void main(){
     vec2 uv = gl_PointCoord;
+    vec4 texColor = texture2D(uTexture, vUv);
     float distanceToCenter = distance(uv, vec2(0.5));
     float alpha = 0.05/distanceToCenter - 0.1;
-    gl_FragColor = vec4(1.0, 1.0, 1.0, alpha);
+    gl_FragColor = vec4(texColor.rgb, alpha);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
--- first/src/shaders/vertex.glsl
+++ first/src/shaders/vertex.glsl
@@ -2,6 +2,7 @@ attribute vec3 aPosition;
 uniform float uProgress;
 uniform vec2 uResolution;
 uniform float uSize;
+varying vec2 vUv;
 
 void main() {
     vec3 mixPos = mix(aPosition, position, uProgress);
@@ -9,5 +10,7 @@ void main() {
     gl_Position = projectionMatrix * modelViewMatrix * vec4(mixPos, 1.0);
     gl_PointSize = uSize * uResolution.y;
     gl_PointSize *= 1.0/gl_Position.z;
-    
+
+    vUv = uv;
+
 }
